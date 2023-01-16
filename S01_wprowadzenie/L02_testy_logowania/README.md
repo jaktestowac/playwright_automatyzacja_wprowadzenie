dc8399468ec8ddf67bc910a811333eeceaee4215
kk <kkijas@jaktestowac.pl>
2023-01-16 17:24:58 +0100

    Add S01L02

--- S01_wprowadzenie/L02_testy_logowania/README.md
+++ S01_wprowadzenie/L02_testy_logowania/README.md
@@ -1,9 +1,10 @@
-# Test Automation training form jaktestowac.pl
+# Test Automation training from jaktestowac.pl
 
 ## Links
 - course https://jaktestowac.pl/course/playwright-wprowadzenie/
 - test site https://demo-bank.vercel.app/  
-if link is broken check https://jaktestowac.pl/lesson/pw1s01l01/
+if link is broken check https://jaktestowac.pl/lesson/pw1s01l02/
+- code repository https://github.com/jaktestowac/playwright_automatyzacja_wprowadzenie
 
 ## Commands
 - check `NodeJS` version  
@@ -22,7 +23,7 @@ if link is broken check https://jaktestowac.pl/lesson/pw1s01l01/
 ## Playwright Config modifications
 - config file `playwright.config.ts`
 - disable browsers, i.e. Firefox  
-    ```json
+    ```javascript
     // {
     //   name: 'firefox',
     //   use: {
@@ -30,3 +31,9 @@ if link is broken check https://jaktestowac.pl/lesson/pw1s01l01/
     //   },
     // },
     ```
+
+## Visual Studio Code
+- Preview: for README.md
+- Autosave: in File -> Auto Save
+- Timeline: file context menu
+- Formatting: editor -> context menu -> Format document
---
+++ S01_wprowadzenie/L02_testy_logowania/tests/login.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('User login to Demobank', () => {
+  
+  test('successful login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLO');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('10987654');
+    await page.getByTestId('login-button').click();
+    await page.getByTestId('user-name').click();
+
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+  test('unsuccessful login with to short username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('error-login-id').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+
+  test('unsuccessful login with to short password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLO');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('1234');
+    await page.getByTestId('password-input').blur();
+
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+
+});
