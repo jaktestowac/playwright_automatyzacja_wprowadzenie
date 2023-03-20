c8728f9d440658bb9f286b039aced8c98ac98324
Przemek B <przemek@jaktestowac.pl>
2023-03-20 13:13:25 +0000

    feat: init s02l07 base

---
+++ S02_uspawnianie_kodu_i_debugowanie/L07_debug/tests/login.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('User login to Demobank', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+  });
+
+  test('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const userId = 'testerLO';
+    const userPassword = '10987654';
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    // Arrange
+    const incorrectUserId = 'tester';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+
+    // Act
+    await page.getByTestId('login-input').fill(incorrectUserId);
+    await page.getByTestId('password-input').click();
+
+    // Assert
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorMessage
+    );
+  });
+
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // Arrange
+    const userId = 'testerLO';
+    const incorrectPassword = '1234';
+    const expectedErrorMessage = 'hasło ma min. 8 znaków';
+
+    // Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(incorrectPassword);
+    await page.getByTestId('password-input').blur();
+
+    // Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectedErrorMessage
+    );
+  });
+});
