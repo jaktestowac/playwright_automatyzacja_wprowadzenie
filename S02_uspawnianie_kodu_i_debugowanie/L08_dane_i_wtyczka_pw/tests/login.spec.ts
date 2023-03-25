26761561a3a98062a3caa51c674ff691115f9849
Przemek B <przemek@jaktestowac.pl>
2023-03-25 06:45:51 +0000

    feat: init s02l08 code

---
+++ S02_uspawnianie_kodu_i_debugowanie/L08_dane_i_wtyczka_pw/tests/login.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+
+test.describe('User login to Demobank', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    // Arrange
+    const incorrectUserId = 'tester';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+
+    // Act
+    await page.getByTestId('login-input').fill(incorrectUserId);
+    await page.getByTestId('password-input').click();
+
+    // Assert
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorMessage
+    );
+  });
+
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // Arrange
+    const userId = loginData.userId;
+    const incorrectPassword = '1234';
+    const expectedErrorMessage = 'hasło ma min. 8 znaków';
+
+    // Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(incorrectPassword);
+    await page.getByTestId('password-input').blur();
+
+    // Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectedErrorMessage
+    );
+  });
+});
---
+++ S02_uspawnianie_kodu_i_debugowanie/L08_dane_i_wtyczka_pw/tests/payment.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+
+test.describe('Payment tests', () => {
+  test.beforeEach(async ({ page }) => {
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+
+    await page.goto('/');
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+
+    await page.getByRole('link', { name: 'płatności' }).click();
+  });
+
+  test('simple payment', async ({ page }) => {
+    // Arange
+    const transferReceiver = 'Jan Nowak';
+    const transferAccount = '12 3456 7890 1234 5678 9012 3456-';
+    const transferAmount = '222';
+    const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla Jan Nowak`;
+
+    // Act
+    await page.getByTestId('transfer_receiver').fill(transferReceiver);
+    await page.getByTestId('form_account_to').fill(transferAccount);
+    await page.getByTestId('form_amount').fill(transferAmount);
+    await page.getByRole('button', { name: 'wykonaj przelew' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(
+      page.getByRole('link', { name: expectedMessage })
+    ).toBeVisible();
+  });
+});
