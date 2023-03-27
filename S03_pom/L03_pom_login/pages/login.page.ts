941ba87b0502c748b98fa2e0f8a93d684c1c72c6
Krzysiek <kkijas@jaktestowac.pl>
2023-03-27 09:29:28 +0200

    feat: init s03l03 code

---
+++ S03_pom/L03_pom_login/pages/login.page.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  loginInput = this.page.getByTestId('login-input');
+  passwordInput = this.page.getByTestId('password-input');
+  loginButton = this.page.getByTestId('login-button');
+}
