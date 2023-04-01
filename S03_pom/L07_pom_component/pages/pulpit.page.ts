b2f02b2029c5ca4a2e307d832cfb7bff11acb0d4
Krzysiek <kkijas@jaktestowac.pl>
2023-04-01 23:26:42 +0200

    feat: init code for s03l07

---
+++ S03_pom/L07_pom_component/pages/pulpit.page.ts
@@ -0,0 +1,29 @@
+import { Page } from '@playwright/test';
+import { SideMenuComponent } from '../component/side-menu.component';
+
+export class PulpitPage {
+  constructor(private page: Page) {}
+
+  sideMenuComponent = new SideMenuComponent(this.page)
+
+  transferReceiverInput = this.page.locator('#widget_1_transfer_receiver');
+  transferAmountInput = this.page.locator('#widget_1_transfer_amount');
+  transferTitleInput = this.page.locator('#widget_1_transfer_title');
+
+  transferButton = this.page.getByRole('button', { name: 'wykonaj' });
+  actionCloseButton = this.page.getByTestId('close-button');
+
+  messageText = this.page.locator('#show_messages');
+
+  topupReceiverInput = this.page.locator('#widget_1_topup_receiver');
+  topupAmountInput = this.page.locator('#widget_1_topup_amount');
+  topupAgreementCheckbox = this.page.locator(
+    '#uniform-widget_1_topup_agreement span'
+  );
+  topupExecuteButton = this.page.getByRole('button', {
+    name: 'doładuj telefon',
+  });
+
+  moneyValueText = this.page.locator('#money_value');
+  userNameText = this.page.getByTestId('user-name')
+}
---
+++ S03_pom/L07_pom_component/tests/payment.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
+import { PaymentPage } from '../pages/payment.page';
+import { PulpitPage } from '../pages/pulpit.page';
+
+test.describe('Payment tests', () => {
+  test.beforeEach(async ({ page }) => {
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+
+    await page.goto('/');
+    const loginPage = new LoginPage(page)
+    await loginPage.loginInput.fill(userId)
+    await loginPage.passwordInput.fill(userPassword)
+    await loginPage.loginButton.click()
+
+    const pulpitPage = new PulpitPage(page)
+    await pulpitPage.sideMenuComponent.paymentLink.click();
+  });
+
+  test('simple payment', async ({ page }) => {
+    // Arrange
+    const transferReceiver = 'Jan Nowak';
+    const transferAccount = '12 3456 7890 1234 5678 9012 34568';
+    const transferAmount = '222';
+    const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla Jan Nowak`;
+
+    // Act
+    const paymentPage = new PaymentPage(page)
+    await paymentPage.transferReceiverInput.fill(transferReceiver)
+    await paymentPage.transferToInput.fill(transferAccount)
+    await paymentPage.transferAmountInput.fill(transferAmount)
+
+    await paymentPage.transferButton.click()
+    await paymentPage.actionCloseButton.click()
+
+    // Assert
+    await expect(paymentPage.messageText).toHaveText(expectedMessage);
+  });
+});
