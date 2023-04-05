a2881bed1a086472da52a16c1d552d1bcf9a4351
Krzysiek <kkijas@jaktestowac.pl>
2023-04-05 21:53:44 +0200

    feat: init code for S03L09

---
+++ S03_pom/L09_pom_aggregate_refactor/pages/pulpit.page.ts
@@ -0,0 +1,29 @@
+import { Page } from '@playwright/test';
+import { SideMenuComponent } from '../component/side-menu.component';
+
+export class PulpitPage {
+  constructor(private page: Page) {}
+
+  sideMenuComponent = new SideMenuComponent(this.page);
+
+  transferReceiverInput = this.page.locator('#widget_1_transfer_receiver');
+  transferAmountInput = this.page.locator('#widget_1_transfer_amount');
+  transferTitleInput = this.page.locator('#widget_1_transfer_title');
+
+  transferButton = this.page.getByRole('button', { name: 'wykonaj' });
+  actionCloseButton = this.page.getByTestId('close-button');
+
+  messageText = this.page.locator('#show_messages');
+
+  topUpReceiverInput = this.page.locator('#widget_1_topup_receiver');
+  topUpAmountInput = this.page.locator('#widget_1_topup_amount');
+  topUpAgreementCheckbox = this.page.locator(
+    '#uniform-widget_1_topup_agreement span'
+  );
+  topUpExecuteButton = this.page.getByRole('button', {
+    name: 'doładuj telefon',
+  });
+
+  moneyValueText = this.page.locator('#money_value');
+  userNameText = this.page.getByTestId('user-name');
+}
